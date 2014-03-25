173c08814f7c6c4e7f85b250e77d1be648da9568
Phillip Ridlen <phillip@ovenbits.com>
2014-03-25 16:42:34 -0500

    Add scoring for snail sort

--- spec/kata/score_spec.rb
+++ spec/kata/score_spec.rb
@@ -1,14 +1,97 @@
 require "./spec/spec_helper"
+require "bundler"
+Bundler.require
 
-$points = {}
+$redis = Redis.new
+RUN_ID = Time.now.to_i
+
+def points(key, incr = nil)
+  key = "kata_competition:#{ RUN_ID }:#{ key }"
+  total = "kata_competition:#{ RUN_ID }:points"
+
+  if incr
+    $redis.incrby(key, incr)
+    $redis.incrby(total, incr)
+  else
+    $redis.get(key)
+  end
+end
+
+points(:snail_sort, 0)
+points(:palindrome, 0)
+points(:calculator, 0)
+
+MiniTest::Unit.after_tests do
+  puts "\n\n"
+  puts "= POINTS ==========================="
+  puts "Snail sort:              #{ points(:snail_sort).to_s.rjust(10) }"
+  puts "Palindrome validator:    #{ points(:palindrome).to_s.rjust(10) }"
+  puts "Method chain calculator: #{ points(:calculator).to_s.rjust(10) }"
+  puts "------------------------------------"
+  puts "Total Points:            #{ $redis.get("kata_competition:#{ RUN_ID }:points").to_s.rjust(10) }"
+end
 
 describe Kata do
+
+  describe "::snail_sort" do
+    it "should sort a 5x5 two-dimensional array" do
+      array = [[  1,  2,  3,  4,  5 ],
+               [  6,  7,  8,  9, 10 ],
+               [ 11, 12, 13, 14, 15 ],
+               [ 16, 17, 18, 19, 20 ],
+               [ 21, 22, 23, 24, 25 ]]
+
+      expected_result = [ 1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19, 18, 17, 12, 13 ]
+
+      Kata.snail_sort(array).must_equal(expected_result)
+
+      points(:snail_sort, 7)
+    end
+
+    it "should fill in nils for sub-arrays that are not as long as the longest sub-array" do
+      array = [[ 1, 2 ],
+               [ 3, 4, 5 ],
+               [ 6 ]]
+
+      expected_result = [ 1, 2, nil, 5, nil, nil, 6, 3, 4 ]
+
+      Kata.snail_sort(array).must_equal(expected_result)
+
+      points(:snail_sort, 4)
+    end
+
+    it "should snail sort arrays that are not square" do
+      array = [[ 1, 2, 3, 4],
+               [ 5, 6, 7, 8]]
+
+      expected_results = [ 1, 2, 3, 4, 8, 7, 6, 5 ]
+
+      Kata.snail_sort(array).must_equal(expected_results)
+
+      points(:snail_sort, 4)
+    end
+
+    it "should sort a large, not-square array with sub-arrays of various lengths" do
+      array = [[  15, 77, 75, 44, 97, 12, 36, 35, 81, 21 ],
+               [ "33", "27", "51", "19", "74", "71", "40" ],
+               [ 1, 2, 7, "hello", 99, Kata, "rubyftw", "for the win" ],
+               [ Minitest, Integer, "\"", 999999999, ["a","b"] ]]
+
+      expected_result = [15, 77, 75, 44, 97, 12, 36, 35, 81, 21, nil, nil, nil, nil, nil, nil, nil,
+                         ["a", "b"], 999999999, "\"", Integer, Minitest, 1, "33",
+                         "27", "51", "19", "74", "71", "40", nil, nil, nil, "for the win",
+                         "rubyftw", Kata, 99, "hello", 7, 2]
+
+      Kata.snail_sort(array).must_equal(expected_result)
+
+      points(:snail_sort, 10)
+    end
+  end
+
+
+  describe "::palindrome?" do
+
+  end
+
 end
-
-puts $points.map do |kata, points|
-  "#{ kata }: #{ points } points\n"
-end
-
-puts "Total Points: #{ $points.values.inject(&:+) }"
-
 
