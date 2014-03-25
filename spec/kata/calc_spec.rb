235d776de70ff1c3cad4f9a150bb61c5edd5d8db
Phillip Ridlen <phillip@ovenbits.com>
2014-03-25 16:58:11 -0500

    Update Calc test cases

--- spec/kata/calc_spec.rb
+++ spec/kata/calc_spec.rb
@@ -2,19 +2,23 @@ require "./spec/spec_helper"
 
 describe Kata::Calc do
   it "should add two numbers" do
-    Calc.new.seven.plus.five.must_equal(12)
+    Kata::Calc.new.seven.plus.five.must_equal(12)
   end
 
   it "should subtract two numbers" do
-    Calc.new.nineteen.minus.five.must_equal(14)
+    Kata::Calc.new.nineteen.minus.five.must_equal(14)
   end
 
   it "should multiply two numbers" do
-    Calc.new.eleven.times.eleven.must_equal(121)
+    Kata::Calc.new.eleven.times.eleven.must_equal(121)
   end
 
   it "should divide two numbers" do
-    Calc.new.twenty.divided_by.five.must_equal(4)
+    Kata::Calc.new.twenty.divided_by.five.must_equal(4)
+  end
+
+  it "should go negative when subtracting a smaller number" do
+    Kata::Calc.new.three.minus.seventeen.must_equal(-14)
   end
 end
 
