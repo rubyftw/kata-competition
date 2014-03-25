ca87216cf93c84009103ecd6784b941d6b748918
Phillip Ridlen <phillip@ovenbits.com>
2014-03-25 16:47:00 -0500

    Add a couple more test cases for the snail sort

--- spec/kata/snail_sort_spec.rb
+++ spec/kata/snail_sort_spec.rb
@@ -10,5 +10,20 @@ describe Kata do
 
       Kata.snail_sort(array).must_equal([1, 2, 3, 6, 9, 8, 7, 4, 5])
     end
+
+    it "should allow non-square arrays" do
+      array = [[ 1, 2, 3 ],
+               [ 4, 5, 6 ]]
+
+      Kata.snail_sort(array).must_equal([1, 2, 3, 6, 5, 4])
+    end
+
+    it "should allow arrays of different sub-lengths, using the longest as the length" do
+      array = [[ 1, 2, 3, 4, 5 ],
+               [ 7, 8, 9, 10, nil ],
+               [ 11, 12, 13, nil, nil ]]
+
+      Kata.snail_sort(array).must_equal([1, 2, 3, 4, 5, 6, nil, nil, nil, 13, 12, 11, 7, 8, 9, 10 ])
+    end
   end
 end
